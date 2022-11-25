0b5588293f8dba0a2526f2124dada801c7c425c7
aigerimass <assylkhanova0@gmail.com>
2022-11-25 16:55:00 +0300

    Added table with all links

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -1,9 +1,16 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.LinkModel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -13,8 +20,32 @@ public class MainController {
     // Model -- обязательный параметр.
     public String home(Model model) {
         model.addAttribute("title", "Main page");
+        model.addAttribute("links", getDummyLinks());
         // это шаблон home
         return "home";
+    }
+
+    private List<LinkModel> getDummyLinks() {
+        return List.of(
+                new LinkModel("google",
+                        "https://www.google.com",
+                        "site google for searching"),
+                new LinkModel("google1",
+                        "google2.com",
+                        "site google for searching"),
+                new LinkModel("google2",
+                        "google3.com",
+                        "site google for searching"),
+                new LinkModel("google3",
+                        "google4.com",
+                        "site google for searching"),
+                new LinkModel("google5",
+                        "google5.com",
+                        "site google for searching"),
+                new LinkModel("google6",
+                        "google6.com",
+                        "site google for searching")
+        );
     }
 
 }
