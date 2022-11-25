ff35fd146bd7fc58f5fd58ab43fc9c1a296379aa
diffitask <ginger.ll@mail.ru>
2022-11-25 15:55:34 +0100

    fix storage file path

--- src/main/java/com/example/demo/models/LinkService.java
+++ src/main/java/com/example/demo/models/LinkService.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 
 public class LinkService implements LinkServiceInterface {
-    private final String storageFileUrl = "./data-storage/users-link-lists-storage.json";
+    private final String storageFileUrl = "../data-storage/users-link-lists-storage.json";
 
     public LinkService() {}
 
